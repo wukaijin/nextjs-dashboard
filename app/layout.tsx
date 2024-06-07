6cf4af8794f51dd3eca02eaaa1f23e344b4d37e4
carlos <568187512@qq.com>
2024-06-07 15:28:13 +0800

    feat: till chapter 5
    Signed-off-by: carlos <568187512@qq.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import '@/app/ui/global.css';
+import { inter, lusitana } from './ui/fonts';
+import clsx from 'clsx';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={clsx(lusitana.className, 'antialiased')}>
+        {children}
+      </body>
+      {/* <body>{children}</body> */}
     </html>
   );
 }
