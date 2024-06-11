3987d4c84f2383dc0d52b316e5c69af0806c55df
carlos <568187512@qq.com>
2024-06-11 14:43:50 +0800

    feat: till chapter 9
    Signed-off-by: carlos <568187512@qq.com>

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -35,6 +35,7 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   noStore();
+  // await new Promise((resolve) => setTimeout(resolve, 300000));
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
